81b3ed7a2f38adeeafd3652797c5a443e2ac90f3
couragenki <genki.meado@gmail.com>
2023-08-17 13:25:48 +0900

    feat: 会話の内容からモバイルキャリアの情報を抽出する

--- app.js
+++ app.js
@@ -34,10 +34,17 @@ const postAsGptBot = async ({ client, channel, threadTs, text }) => {
   });
 };
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openAIClient = new OpenAIApi(configuration);
+
 app.event("message", async ({ event, client, logger }) => {
-  console.log("イベント取得done");
-  // console.log(event, "event");
   const { thread_ts: threadTs, bot_id: botId, text } = event;
+
+  console.log("イベント取得done", text, "text");
+
   // botの返信またはスレッドのメッセージでなければ何もしない
   if (botId || !threadTs) {
     return;
@@ -79,7 +86,59 @@ app.event("message", async ({ event, client, logger }) => {
     console.log(prevMessageText, "prevMessageText");
     console.log(text, "text");
 
-    // 回答メッセージの作成 with OpenAI
+    const mobileTypePrompt = `
+## 前提
+あなたは優秀なスマートフォンキャリアのアドバイザーです。今回顧客から以下のような質問を受けました。そこであなたは以下のモバイルキャリアの情報とユーザーのメッセージをもとに、質問からどのスマホキャリアに関する内容を選別しなさい。
+その際回答には以下で選定するモバイルキャリア候補の中から答えなさい。
+
+## モバイルキャリア候補
+以下の中からモバイルキャリアを選択しなさい。その際以下の10個の選択肢から選定しなさい。
+もしも1~9の選択肢に該当するものがなければ『10. 該当なし』を選択しなさい。
+
+1. docomo
+2. SoftBank
+3. au
+4. 楽天モバイル
+5. ahamo
+6. povo
+7. LINEMO
+8. UQ mobile
+9. Y!mobile
+10. 該当なし
+
+## 回答例
+例えば以下のような質問があったとします。
+
+サンプルの質問：『楽天モバイルの料金プランはどれがおすすめですか？』
+
+上記のような質問場合は以下の回答を行いなさい。
+
+正しい回答：楽天モバイル
+
+回答の際は『カテゴリー: 楽天モバイル』など余計な文字列は付け加えてはいけない。
+必ず『楽天モバイル』とカテゴリー名のみを返すこと。
+
+## 今回カテゴライズする情報
+さて本番です。今回の質問から上記の10つのカテゴリーのうち、どれに当てはまるかを選び、そのカテゴリーのみを出力しなさい。
+
+### 今回の質問:
+『${text}』
+`;
+    // ここで会話の内容からモバイルキャリアの情報抽出
+    const checkMobileType = await openAIClient.createCompletion({
+      model: "text-davinci-003",
+      prompt: mobileTypePrompt,
+      max_tokens: 1000,
+      top_p: 0.5,
+      frequency_penalty: 1,
+    });
+
+    // 抽出結果を入力する
+    const mobileType = checkMobileType.data.choices[0].text;
+
+    console.log(mobileType, "mobileType");
+
+    // 回答メッセージの作成
     const prompt = `
 あなたは優秀なスマートフォンキャリアのアドバイザーです。
 あなたの知識とこれまでの会話の内容を考慮した上で、ユーザーにとってもっと適切なスマートフォンのキャリアに対する質問に答えなさい。
@@ -93,10 +152,6 @@ ${text}
 ### 今の質問の回答:
 `;
 
-    const configuration = new Configuration({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
-    const openAIClient = new OpenAIApi(configuration);
     const completions = await openAIClient.createCompletion({
       model: "text-davinci-003",
       prompt: prompt,
@@ -116,13 +171,15 @@ ${text}
     // 回答メッセージを投稿する
     if (message) {
       console.log("実行まではできたよ");
+
+      デバッグ用に投稿を一時コメントアウトで停止;
       await postAsGptBot({
         client,
         channel: event.channel,
         threadTs,
         text: message,
       });
-      // await say(message);
+      await say(message);
       console.log("発言DONE");
     } else {
       throw new Error("message is empty");
